d631949ac3da2865f4078854ee89a5d5bb3f0a53
syuzyova <lerayuoi@gmail.com>
2020-04-09 13:08:28 +0400

    fixed save domains to db

--- src/main/java/com/funbox/testapplication/service/VisitorServiceBean.java
+++ src/main/java/com/funbox/testapplication/service/VisitorServiceBean.java
@@ -6,7 +6,6 @@ import com.funbox.testapplication.model.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import java.util.*;
 import java.util.regex.Pattern;
@@ -21,16 +20,29 @@ public class VisitorServiceBean implements VisitorService {
 
     public ResponseDto createVisitedLinks(LinksDto linksDto, Long receivedTime) {
         Map<String, Long> map = new HashMap<>();
-        linksDto.getLinks().forEach(link -> {
-            map.put(link, receivedTime);
+        Set<String> domains = new HashSet<>();
+
+        linksDto.getLinks().forEach(k -> {
+            domains.add(Pattern.compile(PATTERN)
+                    .matcher(k)
+                    .results()
+                    .map(m -> m.group(4))
+                    .findAny()
+                    .orElse(null));
         });
+
+        domains.stream()
+                .filter(Objects::nonNull)
+                .forEach(link -> {
+                    map.put(link, receivedTime);
+                });
+
         redisTemplate.opsForHash().putAll("domains", map);
         return new ResponseDto<>();
     }
 
     @Override
     public DomainsDto getVisitedDomains(Long from, Long to) {
-        List<String> result = new ArrayList<>();
         Map<String, Long> domains = new HashMap<>();
         Map<String, Long> finalDomains = domains;
         redisTemplate.opsForHash().entries("domains").forEach((k, v) -> {
@@ -43,17 +55,6 @@ public class VisitorServiceBean implements VisitorService {
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         }
 
-        if (!CollectionUtils.isEmpty(domains)){
-            domains.keySet().forEach(k -> {
-                result.add(Pattern.compile(PATTERN)
-                        .matcher(k)
-                        .results()
-                        .map(m -> m.group(4))
-                        .findAny()
-                        .orElse(null));
-            });
-        }
-
-        return new DomainsDto(result.stream().filter(Objects::nonNull).collect(Collectors.toList()));
+        return new DomainsDto(new ArrayList<>(domains.keySet()));
     }
 }
